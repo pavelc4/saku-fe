a7db76eb51e1d5024da42ddf8df2308a53593a8f
Pavelc4 <pavelc4@users.noreply.github.com>
2026-04-21 14:26:47 +0700

    style: fix toggle switch off state visibility

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -52,7 +52,7 @@
                     <div class="flex items-center gap-6" @click.stop>
                       <label class="relative inline-flex items-center cursor-pointer">
                         <input type="checkbox" class="sr-only peer" v-model="category.active" />
-                        <div class="w-11 h-6 bg-surface-dim peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"></div>
+                        <div class="w-11 h-6 bg-outline-variant peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"></div>
                       </label>
                     </div>
                   </div>
